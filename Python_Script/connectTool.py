d2a35d7e9b7e577bbdb942b6fd2886bb49f32e21
REDWOLF <worakan9012@gmail.com>
2024-04-27 16:58:25 +0700

    BACKUP 4-27-2024
    backup and update README

--- Python_Script/connectTool.py
+++ Python_Script/connectTool.py
@@ -2,8 +2,12 @@
 this python script required python version 3.10 or above
 run this block for chack your python version
 
-recommend used used Jupyter notebook or Google Colab
+normally I'm write firmware in C for embedded device only I'm not expert Python and modern high-level computer language
+some time you will find C writing style in this code and not have any OOP in this 
+(Of course! generally micro-controller it not support OOP ;w;)
 
+I hope someone can write it better than me and fork this project for make it better
+thank you (OwO)b  
 
 AUTHOR REDWOLF DiGiTAL [C.Worakan]
 github : https://github.com/redwolf-digital
@@ -41,7 +45,6 @@ class textColor :
 
 # system and module import
 import platform
-import math
 
 try :
     import numpy as np
@@ -67,7 +70,16 @@ except ImportError as e :
 
 
 
-import random
+import random   # Test only
+
+
+ipTable = {
+        1 : "192.168.1.10",
+        2 : "192.168.1.11",
+        3 : "192.168.1.12",
+        4 : "192.168.1.13",
+        5 : "192.168.1.14",
+}
 
 
 # function
@@ -79,21 +91,36 @@ def connectBoard(boardID, port = 7) :
             0 - when connect fail
     """
 
-    ipTable = {
-        0 : "127.0.0.1",        # loop back-ip for test
-        1 : "192.168.1.10",
-        2 : "192.168.1.11",
-        3 : "192.168.1.12",
-        4 : "192.168.1.13",
-        5 : "192.168.1.14",
-    }
-
     ## code for connect TCP/IP
     if(boardID == 0) :
         telnetlib.Telnet(ipTable[0], 23, timeout=1000)
 
     else :
         telnetlib.Telnet(ipTable[boardID], port, timeout=1000)
+
+
+# function for main task
+def mainProcess(connFlag, buffSize, temp) :
+    packetCounter = 0
+    while(connFlag == 1) :
+            # required data from Cora board and put it in buffer
+            temp = np.empty((0, 0))            # clear buffer
+
+
+            # test case add data to buffer
+            for i in range(buffSize) :
+                n = int(random.randint(0, 65535))
+                temp = np.append(temp, [n])
+
+            packetCounter = packetCounter + 1
+            print("Packet count : ", packetCounter)
+
+            plt.clf()
+            plt.title("Packet Count : " + str(packetCounter))
+            plt.xlabel("smaple block")
+            plt.ylabel("value of sample")
+            plt.plot(temp)
+            plt.pause(1)
 
 
 # function for check in put is integer or not (I hate dynamic variable type ;w;)
@@ -105,16 +132,27 @@ def isInt(x) :
         return False
 
 
+# print dashline UwU
+def dashline() :
+    print("--------------------------------------------")
 
-# main script
 
+
+
+
+
+
+
+
+
+#------------------------------------------------------------------------------------
+#                                      main script
+#------------------------------------------------------------------------------------
 def main() :
-    print(textColor.OKCYAN + "current python version : ", platform.python_version() + textColor.ENDC)
+    print(textColor.OKCYAN + "current python version : ", platform.python_version() + textColor.ENDC, "\n")
 
     NET_PORT = 7
     Net_connFlag = 0
-
-    packetCounter = 0
     max_buffsize = 130
 
 
@@ -125,49 +163,47 @@ def main() :
         # conditions make sure no idiot one input the system doesn't need
         while(Net_connFlag == 0) :
 
-            BoardTargetID = input("Connect to board ?\n")
+            BoardTargetID = input("Command or Connect to board ID ?\n>> ")
+
 
             if isInt(BoardTargetID) == False :
-                print(textColor.FAIL + "INTEGER ONLY" + textColor.ENDC)
+                if  BoardTargetID == "-l" or BoardTargetID == "list" :
+                    dashline()
+                    for i in range(len(ipTable)) :
+                        print(f"[board ID] {i+1} IP : {ipTable[i+1]}")
+                    dashline()
+
+                elif BoardTargetID == "--h" or BoardTargetID == "help":
+                        dashline()
+                        print("--h or help\t Help menu\n-l or list\t Show all board list\n\n press \"Ctrl+C\" for exit program")
+                        dashline()
+                else :
+                    print(textColor.FAIL + "COMMAND NOT FOUND OR INPUT INTEGER ONLY" + textColor.ENDC)
+
+
             else :
                 if int(BoardTargetID) > 0 and int(BoardTargetID) <= 5 :
-                    
                     # connect internet here
-                    print(textColor.WARNING + "----> PRESS \"Ctrl+C\" PROGRAMS <----" + textColor.ENDC)
-        
+
+
+
+
+
+                    print(textColor.WARNING + "----> PRESS \"Ctrl+C\" FOR EXIT PROGRAMS <----" + textColor.ENDC)
                     # set flag to 1 if connect succeed
                     Net_connFlag = 1
 
                 else :
                     print(textColor.FAIL + "OUT OF RANGE" + textColor.ENDC)
-
+            
 
         
 
 
 
         # connect Cora board succeed
-        while(Net_connFlag == 1) :
-            # required data from Cora board and put it in buffer
-            memTemp = np.empty((0, 0))            # clear buffer
-
-
-            # test case add data to buffer
-            for i in range(max_buffsize) :
-                n = int(random.randint(0, 65535))
-                memTemp = np.append(memTemp, [n])
-
-            packetCounter = packetCounter + 1
-            print("Packet counter : ", packetCounter)
-
-            plt.clf()
-            plt.title("Packet Count : " + str(packetCounter))
-            plt.xlabel("smaple block")
-            plt.ylabel("value of sample")
-            plt.plot(memTemp)
-            plt.pause(1)
-    
-    
+        # run main process
+        mainProcess(Net_connFlag, max_buffsize, memTemp)
 
     
     except KeyboardInterrupt :
--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # ENE330Tools
 repository software/script for ENE330 embedded system design
+
+> [!CAUTION]
+> I not warranty ***source code/software*** in this repo it can run perfectly
+>
+> If you find any bugs or any thing else I trust you can fix it by yourself :trollface:
+
+## connectTool
+**Request Python module**  
+*make sure you install in your computer*
+- numpy
+- matplotlib
+- telnetlib3  
+
+### install Python Module
+Windows and linux
+```cli
+pip install numpy
+pip install matplotlib
+pip install telnetlib3
+```
+### how to run script
+for **Windows** used CMD, Terminal or Windows PowerShell
+```cli
+py .\connectTool.py
+```
+or
+```cli
+python .\connectTool.py
+```
+</br>
+
+for **Linux** used Terminal
+```cli
+py connectTool.py
+```
+or
+```cli
+python connectTool.py
+```
